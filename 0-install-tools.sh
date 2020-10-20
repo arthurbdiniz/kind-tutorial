58419a3abbbcb5f03d89ffe44475c27b96fe6677
Arthur Diniz <arthurbdiniz@gmail.com>
2020-10-20 18:32:03 -0300

    Fix kubectl get version command
    Signed-off-by: Arthur Diniz <arthurbdiniz@gmail.com>

--- 0-install-tools.sh
+++ 0-install-tools.sh
@@ -12,7 +12,7 @@ kind --version
 curl -LO "https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl"
 chmod +x ./kubectl
 sudo mv ./kubectl /usr/local/bin/kubectl
-kubectl --version
+kubectl version
 
 # Helm3
 curl https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
