d478dd8667df101cec2d78117ab03a59b09fc6de
Arthur Diniz <arthurbdiniz@gmail.com>
2020-10-20 14:32:49 -0300

    Update helm installation in step 0
    Signed-off-by: Arthur Diniz <arthurbdiniz@gmail.com>

--- 0-install-tools.sh
+++ 0-install-tools.sh
@@ -15,6 +15,4 @@ sudo mv ./kubectl /usr/local/bin/kubectl
 kubectl --version
 
 # Helm3
-curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3
-chmod 700 get_helm.sh
-./get_helm.sh
+curl https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
